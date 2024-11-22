35a727da18045dba7d5bd6644fc3fcee4bbf8327
Buroxxx <abubakarkece31@gmail.com>
2024-11-23 00:36:08 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Fungsi untuk menampilkan info kontak
+function showContactInfo() {
+    alert('Email: Abubakarkece31@gmail.com\nTelepon/WA: 083898309503');
+  }
+  
+  // Tambahkan event listener untuk tombol
+  document.getElementById('contactButton').addEventListener('click', showContactInfo);
